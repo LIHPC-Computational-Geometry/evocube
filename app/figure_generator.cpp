e52c253764997bcef456a3fad3af7418e26e9804
Sebastien Mestrallet <sebastien.mestrallet@cea.fr>
2022-03-18 16:07:36 +0100

    Some cleanup

--- app/figure_generator.cpp
+++ app/figure_generator.cpp
@@ -29,35 +29,71 @@
 #include "flagging_utils.h"
 #include "mesh_io.h"
 
+// **********************************************************
+//         PARAMETERS
+
 #define PICTURES_WIDTH  360
 #define PICTURES_HEIGHT 360
 
-#define BOUNDARY_PATH       (saving_subfolder + mesh_name + "/boundary.obj")
-#define POLYCUBE_PATH       (saving_subfolder + mesh_name + "/polycube_surf_int.obj")
-#define FLAGGING_PATH       (saving_subfolder + mesh_name + "/labeling.txt")
-#define FEATURE_EDGES_PATH  (saving_subfolder + mesh_name + "/input_edges.mesh")
+#define DEFAULT_ZOOM                0.9
+#define DEFAULT_TRANSLATION_VECTOR  Eigen::Vector3d(0,0,0)
+#define USE_ORTHOGRAPHIC_PROJ       false
+#define TILT                        0.6
+
+// #define USE_SPECIAL_ROTATION_MATRIX__BUNNY
+// #define USE_SPECIAL_ROTATION_MATRIX__BIMBA
+// #define USE_SPECIAL_ROTATION_MATRIX__B52
+// #define USE_SPECIAL_ROTATION_MATRIX__MAX
+// #define USE_SPECIAL_ROTATION_MATRIX__MOAI
+// #define USE_SPECIAL_ROTATION_MATRIX__ONI
+
+#define DEFAULT_DATA_FODLER     "../data/"
+#define DEFAULT_MODEL           "bunny"
+#define BOUNDARY_PATH(data_folder,mesh_name)        ((data_folder) + (mesh_name) + "/boundary.obj")
+#define POLYCUBE_PATH(data_folder,mesh_name)        ((data_folder) + (mesh_name) + "/polycube_surf_int.obj")
+#define FLAGGING_PATH(data_folder,mesh_name)        ((data_folder) + (mesh_name) + "/labeling.txt")
+#define FEATURE_EDGES_PATH(data_folder,mesh_name)   ((data_folder) + (mesh_name) + "/input_edges.mesh")
+
+#define DEFAULT_FIGURE_TYPE                             FIG_0_POLYCUBE //see enum FIGURE
+#define DEFAULT_OUTPUT_PATH(fig_type,number)                ("../temp_picture" + std::to_string(fig_type) + "_" + std::to_string(number) + ".png")
+#define OUTPUT_PATH(data_folder,mesh_name,fig_type,number)  ((data_folder) + (mesh_name) + "/fig" + std::to_string(fig_type) + "_" + std::to_string(number) + ".png")
+
+// **********************************************************
 
 int main(int argc, char *argv[]) {
 
-    std::string mesh_name = "bunny", expected_mesh = "";
+    // usage: ./figure_generator [model] [figure_type] [data_folder]
+    // examples :
+    //
+    // ./figure_generator B21 1 ../custom/data/folder/
+    //      Will open the B21 folder in ../custom/data/folder/,
+    //      search for the input files,
+    //      generate figures of type 1 (see enum FIGURE),
+    //      and write the pictures there
+    //
+    // ./figure_generator B21 1
+    //      Same, but will open the DEFAULT_DATA_FODLER/B21 folder
+    //
+    // ./figure_generator B21
+    //      Same, but will generate the DEFAULT_FIGURE_TYPE
+    //
+    // ./figure_generator
+    //      Same, but will use the DEFAULT_MODEL
 
-    std::string saving_subfolder = "../data/";
-    //saving_subfolder = "../good_ones/demo_polycubes/";
-    //mesh_name = "S25";
+    std::string mesh_name = DEFAULT_MODEL, data_folder = DEFAULT_DATA_FODLER;
+    double zoom_value = DEFAULT_ZOOM;
+    Eigen::Vector3d trans_vector = DEFAULT_TRANSLATION_VECTOR;
+    int n_figs = -1;
 
     if (argc >= 2){
-        mesh_name = std::string(argv[1]);
+        mesh_name = argv[1];
     }
 
     if (argc >= 4){
-        saving_subfolder = std::string(argv[3]);
+        data_folder = argv[3];
     }
 
-    // --- OPTIONS --- //
-
-    double zoom_value = 0.9;
-    Eigen::Vector3d trans_vector(0, 0, 0);
-    bool is_orthographic = false;
+    // --- options for the figure types --- //
 
     enum MESH_MODE {INITIAL,    //input triangular mesh
                     POLYCUBE    //polycube triangular mesh
@@ -89,95 +125,90 @@ int main(int argc, char *argv[]) {
                  FIG_2_LABELING_CUSTOM_VIEW,
                  FIG_3_NORMALS,
                  FIG_4_LABELING};
-    FIGURE figure = FIG_0_POLYCUBE;
+    FIGURE figure = DEFAULT_FIGURE_TYPE;
 
     if (argc >= 3){
         figure = (FIGURE) std::atoi(argv[2]);
     }
-
-    int n_figs;
-
-    if (figure == FIG_0_POLYCUBE){
-        colors_mode = LABELLING;
-        edges_mode = BOUNDARIES;
-        mesh_mode = POLYCUBE;
-        n_figs = 4;
-        colors_face_AO = true;
-    }
-
-    if (figure == FIG_1_INPUT){
-        colors_mode = WHITE;
-        edges_mode = FEATURES;
-        mesh_mode = INITIAL;
-        n_figs = 4;
-        colors_face_AO = true;
-    }
-
-    if (figure == FIG_2_LABELING_CUSTOM_VIEW){
-        colors_mode = NAIVE_LABELLING;
-        mesh_mode = INITIAL;
-        n_figs = 1;
-        rotation_mode = SINGLE_PRESET;
-        rot_preset = Eigen::MatrixXd(3, 3);
-        rot_preset << 0.886888, -0.459069, 0.0518283,
-                      0.161148,  0.412548,  0.896569,
-                      -0.432969, -0.786804,  0.439861;
-
-    }
-
-    if (figure == FIG_3_NORMALS){
-        // This one also has slight modifications further down
-        colors_mode = NORMALS;
-        mesh_mode = INITIAL;
-        n_figs = 3;
-        rotation_mode = SINGLE_PRESET;
-        rot_preset = Eigen::MatrixXd(3, 3);
-        rot_preset << 0.886888, -0.459069, 0.0518283,
-                      0.161148,  0.412548,  0.896569,
-                      -0.432969, -0.786804,  0.439861;
-    }
-
-    if (figure == FIG_4_LABELING){
-        colors_mode = LABELLING;
-        edges_mode = NO_EDGES; //BOUNDARIES;
-        mesh_mode = INITIAL;
-        n_figs = 4;
-        colors_face_AO = true;
-    }
     
+    switch(figure) {
+
+        case FIG_0_POLYCUBE:
+            colors_mode = LABELLING;
+            edges_mode = BOUNDARIES;
+            mesh_mode = POLYCUBE;
+            n_figs = 4;
+            colors_face_AO = true;
+            break;
+        
+        case FIG_1_INPUT:
+            colors_mode = WHITE;
+            edges_mode = FEATURES;
+            mesh_mode = INITIAL;
+            n_figs = 4;
+            colors_face_AO = true;
+            break;
+
+        case FIG_2_LABELING_CUSTOM_VIEW:
+            colors_mode = NAIVE_LABELLING;
+            mesh_mode = INITIAL;
+            n_figs = 1;
+            rotation_mode = SINGLE_PRESET;
+            rot_preset = Eigen::MatrixXd(3, 3);
+            rot_preset << 0.886888, -0.459069, 0.0518283,
+                        0.161148,  0.412548,  0.896569,
+                        -0.432969, -0.786804,  0.439861;
+            break;
+
+        case FIG_3_NORMALS:
+            // This one also has slight modifications further down
+            colors_mode = NORMALS;
+            mesh_mode = INITIAL;
+            n_figs = 3;
+            rotation_mode = SINGLE_PRESET;
+            rot_preset = Eigen::MatrixXd(3, 3);
+            rot_preset << 0.886888, -0.459069, 0.0518283,
+                        0.161148,  0.412548,  0.896569,
+                        -0.432969, -0.786804,  0.439861;
+            break;
+
+        case FIG_4_LABELING:
+            colors_mode = LABELLING;
+            edges_mode = NO_EDGES; //BOUNDARIES;
+            mesh_mode = INITIAL;
+            n_figs = 4;
+            colors_face_AO = true;
+            break;
+    }
 
     // ---  --- //
 
-    if (expected_mesh != "" && expected_mesh != mesh_name){
-        coloredPrint("ERROR: mesh name doesn't match expected name for this figure", "red");
-    }
-
     Eigen::MatrixXd V;
     Eigen::MatrixXi F;
-
-    if (mesh_mode == INITIAL){
-        std::string mesh_file = BOUNDARY_PATH;
-        igl::read_triangle_mesh(mesh_file, V, F);
-    }
-    else if (mesh_mode == POLYCUBE){
-        std::string mesh_file = POLYCUBE_PATH;
-        igl::read_triangle_mesh(mesh_file, V, F);
-    }    
-
-    std::string input_edges = FEATURE_EDGES_PATH;
+    Eigen::MatrixXi TT;
+    Eigen::MatrixXd N;
     Eigen::MatrixXi edges;
     Eigen::MatrixXd V_edges;
+    std::string mesh_file, feature_edges_file;
+
+    //open the mesh file according to the mesh_mode
+    mesh_file = (mesh_mode == INITIAL) ? BOUNDARY_PATH(data_folder,mesh_name) : POLYCUBE_PATH(data_folder,mesh_name);
+    std::cout << "Reading " << mesh_file << "..." << std::endl;
+    igl::read_triangle_mesh(mesh_file, V, F);
+
+    //open the feature edges file
+    feature_edges_file = FEATURE_EDGES_PATH(data_folder,mesh_name);
     if (edges_mode == FEATURES) {
-        if (!std::ifstream(input_edges.c_str()).good()){
+        if (!std::ifstream(feature_edges_file.c_str()).good()){
             coloredPrint("Feature edges can't be read.", "yellow");
         }
         else {
-            readDotMeshEdges(input_edges, V_edges, edges);
+            readDotMeshEdges(feature_edges_file, V_edges, edges);
         }
     }
 
-    Eigen::MatrixXi TT; 
     igl::triangle_triangle_adjacency(F, TT);
+    igl::per_vertex_normals(V, F, N);
 
     V.col(0) = V.col(0).array() - V.col(0).mean();
     V.col(1) = V.col(1).array() - V.col(1).mean();
@@ -185,34 +216,15 @@ int main(int argc, char *argv[]) {
 
     std::cout << "Mean: " << V.colwise().mean() << std::endl;
 
-
     // --------------------------
 
-    Eigen::MatrixXd N;
-    igl::per_vertex_normals(V, F, N);
     Eigen::VectorXd AO;
     igl::embree::ambient_occlusion(V, F, V, N, 30, AO);
 
-    Eigen::VectorXi flagging(F.rows());
-    std::string flagging_path = FLAGGING_PATH;
-
-    std::ifstream input_file(flagging_path);
-    if (!input_file.is_open()) {
-        std::cerr << "Could not open assignment file: " << flagging_path << std::endl;
-    }
-    else {
-        int assig;
-        int n=0;
-        int number;
-        while (input_file >> number) {
-            flagging(n) = number;
-            n++;
-        }
-        if (n != F.rows()){
-            std::cout << "Error reading flagging" << std::endl;
-        }
-    }
-
+    //open flagging file
+    std::string flagging_file = FLAGGING_PATH(data_folder,mesh_name);
+    std::cout << "Reading " << flagging_file << "..." << std::endl;
+    Eigen::VectorXi flagging = openFlagging(flagging_file,F.rows());
 
     Eigen::VectorXd face_AO(F.rows());
     for (int i=0; i<F.rows(); i++){
@@ -227,21 +239,27 @@ int main(int argc, char *argv[]) {
     }
     smooth_AO = smooth_AO.array() / 4.0;
 
-
+    //compute colors
     Eigen::MatrixXd colors;
+    switch (colors_mode) {
+        
+        case LABELLING:
+            colors = colorsFromFlagging(flagging);
+            break;
 
-    if (colors_mode == LABELLING){
-        colors = colorsFromFlagging(flagging);
-    }
-    else if (colors_mode == WHITE){
-        colors = Eigen::MatrixXd::Constant(F.rows(), 3, 0.8);
-    }
-    else if (colors_mode == NORMALS){
-        colors = N.array().abs();
-    }
-    else if (colors_mode == NAIVE_LABELLING){
-        Eigen::VectorXi naive_flagging = normalFlagging(V, F);
-        colors = colorsFromFlagging(naive_flagging);
+        case WHITE:
+            colors = Eigen::MatrixXd::Constant(F.rows(), 3, 0.8);
+            break;
+
+        case NORMALS:
+            colors = N.array().abs();
+            break;
+
+        case NAIVE_LABELLING: {
+            Eigen::VectorXi naive_flagging = normalFlagging(V, F);
+            colors = colorsFromFlagging(naive_flagging);
+            break; }
+            
     }
 
     if (colors_face_AO){
@@ -257,6 +275,7 @@ int main(int argc, char *argv[]) {
     Eigen::MatrixXf ends(0,0);
     Eigen::MatrixXf edge_colors(0,0);
 
+    //compute edges
     if (edges_mode == BOUNDARIES){
 
         Eigen::MatrixXf bnd_flag_colors(3,3);
@@ -326,96 +345,60 @@ int main(int argc, char *argv[]) {
     std::cout << "Sending mesh to renderer" << std::endl;
     er.set_mesh(V, F, true);
     
-    if (edges_mode != NO_EDGES){
+    if (edges_mode != NO_EDGES) {
         std::cout << "Sending edges to renderer" << std::endl;
         er.set_edges(begs, ends, edge_colors);
         er.set_edges_per_tri(edges_per_tri);
     }
 
-    //er.set_data(K,igl::COLOR_MAP_TYPE_JET);
-
     std::cout << "Sending colors to renderer" << std::endl;
     er.set_colors(colors);
-    /*
-    std::vector<double> rotX = {20, 20, -50, 70};
-    std::vector<double> rotY = {-70, 70, 0, 30};
-    std::vector<double> rotZ = {0, 0, 0, 0};*/
 
-    double tilt = 0.6;
-    std::vector<double> rotX = {45 * tilt, -45 * tilt, -(180 - 45 * tilt), 180 - 45 * tilt};
+    std::vector<double> rotX = {45 * TILT, -45 * TILT, -(180 - 45 * TILT), 180 - 45 * TILT};
     std::vector<double> rotY = {-45, 45, 45, -45};
     std::vector<double> rotZ = {0, 0, 180, 180};
+    Eigen::Matrix3d rot_matrix;
 
-    for (int pic=0; pic<n_figs; pic++){
-        std::cout << "Pic " << pic << std::endl; 
-        
-
-        Eigen::Matrix3d rot_matrix;
+    for (int pic=0; pic<n_figs; pic++) {
+        std::cout << "Pic " << pic << std::endl;
 
         if (rotation_mode == SINGLE_PRESET) {
             rot_matrix = rot_preset;
         }
         else {
-            // specify rotation
-            // -90 X for bunny
             rot_matrix =  Eigen::AngleAxisd((rotX[pic])*igl::PI/180.0, Eigen::Vector3d::UnitX())
                         * Eigen::AngleAxisd((rotY[pic])*igl::PI/180.0, Eigen::Vector3d::UnitY())
                         * Eigen::AngleAxisd((rotZ[pic])*igl::PI/180.0, Eigen::Vector3d::UnitZ());
         }
 
-        // TODO REMOVE: only for bunny
-        bool bunny_special = false;
-        if (bunny_special){
-            rot_preset << 0.857921, -0.513781, -0.00074251,
-                            0.167674, 0.278619, 0.945652,
-                            -0.485651, -0.811419, 0.325181;
-            rot_matrix = rot_preset;
-        }
-
-
-        bool bimba_special = false;
-        if (bimba_special){
-            rot_preset << -0.917247, -0.397913, -0.0179899,
-                        0.149208, -0.385121, 0.910725,
-                        -0.369317, 0.832675, 0.412623;
-
-            rot_matrix = rot_preset;
-        }
-
-        bool B52_special = false;
-        if (B52_special){
-            rot_preset << -0.907966, -0.416895, 0.0423878,
-                            -0.218289, 0.384208, -0.89707,
-                            0.357698, -0.823761, -0.439852;
-            rot_matrix = rot_preset;
-        }
-
-        bool max_special = false;
-        if (max_special){
-            rot_preset << 0.726065, 0.687486, -0.0139064,
-                        0.0716724, -0.0555499, 0.99588,
-                        0.683881, -0.72407, -0.0896065;
-            rot_matrix = rot_preset;
-        }
-
-        bool moai_special = false;
-        if (moai_special){
-            rot_preset << 0.621212, -0.779063, 0.0845922,
-                        0.292434, 0.330615, 0.897316,
-                        -0.727033, -0.532686, 0.433206;
-            rot_matrix = rot_preset;
-        }
-
-        bool oni_special = false;
-        if (oni_special){
-            rot_preset << 0.784854, -0.619391, -0.0189354,
-                    0.120424, 0.122477, 0.985138,
-                    -0.607867, -0.77547, 0.170716;
-            rot_matrix = rot_preset;
-        }
-
+        #if defined(USE_SPECIAL_ROTATION_MATRIX__BUNNY)
+        rot_matrix << 0.857921,  -0.513781, -0.00074251,
+                      0.167674,  0.278619,  0.945652,
+                      -0.485651, -0.811419, 0.325181;
+        #elif defined(USE_SPECIAL_ROTATION_MATRIX__BIMBA)
+        rot_matrix << -0.917247, -0.397913, -0.0179899,
+                      0.149208,  -0.385121, 0.910725,
+                      -0.369317, 0.832675,  0.412623;
+        #elif defined(USE_SPECIAL_ROTATION_MATRIX__B52)
+        rot_matrix << -0.907966, -0.416895, 0.0423878,
+                      -0.218289, 0.384208,  -0.89707,
+                      0.357698,  -0.823761, -0.439852;
+        #elif defined(USE_SPECIAL_ROTATION_MATRIX__MAX)
+        rot_matrix << 0.726065,  0.687486,   -0.0139064,
+                      0.0716724, -0.0555499, 0.99588,
+                      0.683881,  -0.72407,   -0.0896065;
+        #elif defined(USE_SPECIAL_ROTATION_MATRIX__MOAI)
+        rot_matrix << 0.621212,  -0.779063, 0.0845922,
+                      0.292434,  0.330615,  0.897316,
+                      -0.727033, -0.532686, 0.433206;
+        #elif defined(USE_SPECIAL_ROTATION_MATRIX__ONI)
+        rot_matrix << 0.784854,  -0.619391, -0.0189354,
+                      0.120424,  0.122477,  0.985138,
+                      -0.607867, -0.77547,  0.170716;
+        #endif
 
         if (figure == FIG_3_NORMALS){
+            //for this mode the colors depend on the picture number
             Eigen::MatrixXd new_colors = colors;
             new_colors.col(pic) = new_colors.col(pic).array() * new_colors.col(pic).array(); 
             new_colors.col((pic+1)%3) = Eigen::VectorXd::Constant(new_colors.rows(), 0.0);
@@ -427,12 +410,12 @@ int main(int argc, char *argv[]) {
             er.set_colors(new_colors);
         }
 
-
         er.set_rot(rot_matrix);
         er.set_translation(trans_vector);
         er.set_zoom(zoom_value);
-        er.set_orthographic(is_orthographic);
-            
+        er.set_orthographic(USE_ORTHOGRAPHIC_PROJ);
+        
+        //variables for the Red/Green/Blue/Alpha channels
         Eigen::Matrix<unsigned char,Eigen::Dynamic,Eigen::Dynamic> R(PICTURES_WIDTH, PICTURES_HEIGHT);
         Eigen::Matrix<unsigned char,Eigen::Dynamic,Eigen::Dynamic> G(PICTURES_WIDTH, PICTURES_HEIGHT);
         Eigen::Matrix<unsigned char,Eigen::Dynamic,Eigen::Dynamic> B(PICTURES_WIDTH, PICTURES_HEIGHT);
@@ -444,16 +427,9 @@ int main(int argc, char *argv[]) {
         std::cout << "Rendered !" << std::endl;
 
         // save to PNG file
-        
-        std::string png_file = "../temp_picture" + std::to_string(pic) + ".png";
-
-        if (argc >= 2){
-            png_file = saving_subfolder + mesh_name + "/fig" + std::to_string((int) figure) + "_" + std::to_string(pic) + ".png";
-        }
-
-        std::cout<<"Rendered scene saved to "<<png_file<<std::endl;
+        std::string png_file = (argc >= 2) ? OUTPUT_PATH(data_folder,mesh_name,(int) figure,pic) : DEFAULT_OUTPUT_PATH((int) figure,pic);
         igl::png::writePNG(R, G, B, A, png_file);
-
+        std::cout << "Rendered scene saved to "<< png_file << std::endl;
     }
 
     return 0;
